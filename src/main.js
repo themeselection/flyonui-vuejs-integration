56cf98ce0dd20ed74e6f9b06ffa333e47a6dcb9a
yagnikvamja <yvpatel121@gmail.com>
2025-07-22 17:10:09 +0530

    refactor: updated flyonui version

--- src/main.js
+++ src/main.js
@@ -1,19 +1,6 @@
 import { createApp } from "vue";
 import "./index.css";
 
-// Optional third-party libraries
-import $ from "jquery";
-import _ from "lodash";
-import noUiSlider from "nouislider";
-import "datatables.net";
-import "dropzone/dist/dropzone-min.js";
-
-window._ = _;
-window.$ = $;
-window.jQuery = $;
-window.DataTable = $.fn.dataTable;
-window.noUiSlider = noUiSlider;
-
 import "flyonui/flyonui";
 import App from "./App.vue";
 import router from "./router";
